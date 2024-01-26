afc05d9858295c2dc292be1c3517f184ee7a1139
Fabio Machado <camposmachado.fabio@gmail.com>
2024-01-26 15:09:45 +0000

    fix: indentation

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,12 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 import 'dotenv/config';
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-  });
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB,
+	password: process.env.DB_PASSWORD,
+	port: 5432,
+});
   
-  export default db;
+export default db;
